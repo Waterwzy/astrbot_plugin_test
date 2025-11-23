9ac9327604c479aa64a0532f309f588aea6fa5b9
Super User <2916963017@qq.com>
2025-11-23 11:12:31 +0800

    功能更新，bugfix

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 import astrbot.api.message_components as Comp
+from astrbot.api.message_components import ComponentType
 from astrbot.core.message.message_event_result import MessageChain
 import json
 import time
 import random
 import math
 import re
+from operator import itemgetter
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
@@ -23,6 +25,29 @@ class MyPlugin(Star):
                 return special_user
         return None
 
+    def add_favorite(self,favorite_num:int,favorite_user:int,waterlist:list) -> None :
+        flag = 1
+        for user in waterlist['favorite_list'] :
+            if user['id'] == favorite_user :
+                user['favorite'] += favorite_num
+                flag = 0
+                break
+        if flag :
+            waterlist['favorite_list'].append({"id":favorite_user,"favorite":favorite_num})
+        self.write_water(waterlist)
+        return
+
+    def is_at(self,msg_chain: list[Comp.BaseMessageComponent], bot_id: int) -> bool:
+        for msg in msg_chain:
+            if not isinstance(msg, Comp.BaseMessageComponent):
+                continue
+            if msg.type is ComponentType.At:
+                
+                qq = getattr(msg, "qq", None)
+                logger.info(f"检测到含有@消息的消息，消息qq：{qq}")
+                if int(qq) == bot_id:                        # 3. 统一转字符串
+                    return True
+        return False
 
     def is_float(self,target) ->bool :
         try :
@@ -34,13 +59,27 @@ class MyPlugin(Star):
     async def check_date_update(self) :
         waterlist = self.create_waterlist()
         if waterlist['date_mon'] != time.localtime(time.time()).tm_mon or waterlist['date_day'] != time.localtime(time.time()).tm_mday :
-            waterlist['today_water_list'] = []
-            waterlist['date_mon'] = time.localtime(time.time()).tm_mon
-            waterlist['date_day'] = time.localtime(time.time()).tm_mday
-            waterlist['water_boss']['now_hp'] = waterlist['water_boss']['today_hp'] + waterlist['water_boss']['hp_add_of_yesterday']
-            waterlist['water_boss']['today_hp'] = waterlist['water_boss']['now_hp']
-            waterlist['water_boss']['kill_list'] = []
-            waterlist['water_boss']['hp_add_of_yesterday'] = -5
+            try:
+                yesterday_water_hp = waterlist['water_boss']['today_hp']
+                waterlist['water_boss']['kill_list'] = sorted(waterlist['water_boss']['kill_list'] , key=itemgetter('hp'),reverse=True)
+                yesterday_max_id = waterlist['water_boss']['kill_list'][0]['id']
+                yesterday_max_hp = waterlist['water_boss']['kill_list'][0]['hp']
+                waterlist['today_water_list'] = []
+                waterlist['date_mon'] = time.localtime(time.time()).tm_mon
+                waterlist['date_day'] = time.localtime(time.time()).tm_mday
+                waterlist['water_boss']['now_hp'] = waterlist['water_boss']['today_hp'] + waterlist['water_boss']['hp_add_of_yesterday']
+                waterlist['water_boss']['today_hp'] = waterlist['water_boss']['now_hp']
+                waterlist['water_boss']['kill_list'] = []
+                waterlist['water_boss']['hp_add_of_yesterday'] = -5
+                chain = [
+                    Comp.Plain(f"=====昨日打水总计=====\n昨日血量:{yesterday_water_hp}\n昨日打水最多: "),
+                    Comp.At(qq = yesterday_max_id),
+                    Comp.Plain(f"({yesterday_water_hp})\n今日更新血量:{waterlist['water_boss']['today_hp']}")
+                ]
+                
+                await self.context.send_message(waterlist['message_session'],MessageChain(chain))
+            except Exception as e:
+                logger.error(f"catch error in checking update:{e}")
         random.seed(time.time())
         self.write_water(waterlist)
 
@@ -93,7 +132,7 @@ class MyPlugin(Star):
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")
 
-    @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE)
+    @filter.event_message_type(filter.EventMessageType.GROUP_MESSAGE,priority = 20 )
     async def water_in_group(self, event: AstrMessageEvent):
         
         """打水功能"""
@@ -148,6 +187,9 @@ class MyPlugin(Star):
                     Comp.At(qq=event.get_sender_id()),
                     Comp.Plain(f"\u200b\n打水成功！你今日打水{today_count}次。\n你总计打水{sender_count}次。")
                 ]
+
+                self.add_favorite(3,sender_id,waterlist)
+
                 yield event.chain_result(chian)
             except Exception as e:
                 logger.error(f"打水操作出错: {e}")
@@ -182,9 +224,12 @@ class MyPlugin(Star):
                 add_str = '' if kill_more == 1 else '（暴击）'
                 add_str_after = '' if waterlist['water_boss']['now_hp'] != 0 else '(今天的水水被打死了！)'
                 waterlist['water_boss']['kill_list'].append({"id":int(event.get_sender_id()),"hp":fin_kill_hp})
+
+                self.add_favorite(int(-fin_kill_hp*0.1) , sender_id , waterlist)
+
                 chain = [
                     Comp.At(qq=event.get_sender_id()),
-                    Comp.Plain(f" 打水水成功，你今天给水水造成的伤害值是{fin_kill_hp}{add_str}（{waterlist['water_boss']['now_hp']}/{waterlist['water_boss']['today_hp']}）。{add_str_after}{add_out_str}")
+                    Comp.Plain(f"\u200b\n 打水水成功，你今天给水水造成的伤害值是{fin_kill_hp}{add_str}（{waterlist['water_boss']['now_hp']}/{waterlist['water_boss']['today_hp']}）。{add_str_after}{add_out_str}")
                 ]
                 yield event.chain_result(chain)
                 #self.write_water(waterlist)
@@ -203,7 +248,7 @@ class MyPlugin(Star):
             if message_str == "灌水" :
                 yield event.plain_result(f"你现在的灌水可用值：{user_info['hp']}（输入“灌水 具体数值”以增加水水血量，打水伤害与灌水可用值换算比为10:1）")
             elif (not self.is_float(message_str[ 3 : ]) ) or round(float( message_str[3 : ]) , 1) <= 0 :
-                yield event.plain_result(f"参数错误！（请输入正浮点数）")
+                yield event.plain_result(f"参数错误！（请输入正浮点数，灌水后需要有空格）")
             else :
                 add_hp = round(float( message_str[3 : ]) , 1)
                 if user_info['hp'] < add_hp or (waterlist['water_boss']['now_hp'] == 0 and user_info['hp'] -1 < add_hp):
@@ -214,20 +259,37 @@ class MyPlugin(Star):
                     extra_reborn = 1
                 waterlist['water_boss']['now_hp'] = round(waterlist['water_boss']['now_hp']+add_hp , 1 )
                 user_info['hp'] = round(user_info['hp'] - add_hp -extra_reborn, 1 )
+
+                self.add_favorite(int(add_hp*1.5) , sender_id , waterlist)
+
                 chain = [
                     Comp.At(qq = event.get_sender_id()),
-                    Comp.Plain(f"\n灌水成功。你给水水增加的血量是{add_hp},水水目前血量{waterlist['water_boss']['now_hp']}")
+                    Comp.Plain(f"\u200b\n灌水成功。你给水水增加的血量是{add_hp},水水目前血量{waterlist['water_boss']['now_hp']}")
                 ]
                 yield event.chain_result(chain)
+        elif re.search("好感",message_str) is not None and self.is_at(event.message_obj.message,3516791958) :
+            logger.info("触发好感度提示")
+            for user in waterlist['favorite_list'] :
+                if user['id'] == sender_id :
+                    
+                    chain = [
+                        Comp.At(qq = sender_id),
+                        Comp.Plain(f"\u200b 你的好感度是{user['favorite']}喵")
+                    ]
+                    yield event.chain_result(chain)
+                    return
+            yield event.plain_result("你还没有好感度喵？")
         elif self.is_special_call(sender_id,waterlist) is not None :
             logger.info(f"确认为特殊用户")
             target = self.is_special_call(sender_id,waterlist)
             randoms = random.randint(1,target['call_random'])
             if randoms == 1:
+                self.add_favorite(1,sender_id,waterlist)
                 yield event.plain_result(f"{target['call_str']}")
             else :
                 logger.info(f"call_random not worked,now random int:{randoms}")
-                
+
+
         self.write_water(waterlist)
             
     async def terminate(self):
